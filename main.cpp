8162426a46b3c8a8a114c254244e8855292dbe4f
Ilya Krasnyy <krasny.ilya0@gmail.com>
2024-05-16 20:34:39 -0700

    Complete

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 
 int main() {
-vector<int> numArray = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}; //Asumption allowed, always given a vector size that sqrt could be taken of. (4,9,16,25,46,49,64...)
+vector<int> numArray = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}; //Told by Interviewer, asumption allowed: always given a vector size that sqrt could be taken of. (4,9,16,25,46,49,64...)
 snakePattern(numArray);
 
 }
